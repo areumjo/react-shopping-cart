6165e74924fe6da10f733bbc8cbe4eaad749c13f
Areum Jo <areumjo1@gmail.com>
2019-08-12 15:10:12 -0400

    Initial commit: Project set up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
-// Components
+// 3 Components and 2 state (products and card)
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
